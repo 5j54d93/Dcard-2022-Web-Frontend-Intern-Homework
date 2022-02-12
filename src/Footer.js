be018f93f21d550a73abba1db761ffe4a9f6bf85
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-12 23:13:46 +0800

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         </p>
         <p class='mb-0 text-center'>
           Copyright © 2022 Ricky Chuang｜
-          <a href='/Dcard-2022-Web-Frontend-Intern-Homework/'>首頁</a>｜
+          <a href='/'>首頁</a>｜
           <a href='https://linktr.ee/5j_54d93' target='_blank' rel="noreferrer">社群連結</a>｜
           <a href='https://github.com/5j54d93/Dcard-2022-Web-Frontend-Intern-Homework' target='_blank' rel="noreferrer">GitHub</a>｜
           Designed with ❤️ by Ricky Chuang
