a0e430daefc4b7cc15dcbca24900905a8a53cecb
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-15 17:13:17 +0800

    Update RepoList.js

--- src/RepoList.js
+++ src/RepoList.js
@@ -145,7 +145,7 @@ class RepoRow extends Component {
 class BackToHome extends Component {
   render() {
     return (
-      <Link class="btn btn-outline-primary mb-3" to='/' role='button'>
+      <Link class="btn btn-outline-white mb-3" to='/' role='button'>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 20 20">
           <path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z" />
         </svg> Search for another user
