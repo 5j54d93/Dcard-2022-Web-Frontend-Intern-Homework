0067006009334cf669ccbde05fa3348f3d64f947
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-18 17:03:26 +0800

    Add link to RepoList on `RepoOwner`

--- src/RepoDetail.js
+++ src/RepoDetail.js
@@ -57,9 +57,11 @@ RepoOwner.defaultProps = {
 }
 function RepoOwner(props) {
   return (
-    <div className='d-flex align-items-center gap-2 mb-4'>
-      <img className='img-fluid' style={{ borderRadius: '50%' }} src={props.owner.avatar_url} alt={'GitHub user：' + props.owner.login} width='32px' height='32px' />
-      <span className='text-dark-gray fs-5 fw-light me-auto'>{props.owner.login}</span>
+    <div className='d-flex mb-4'>
+      <Link className='d-flex align-items-center gap-2 me-auto' to={'/users/' + props.owner.login + '/repos'}>
+        <img className='img-fluid' style={{ borderRadius: '50%' }} src={props.owner.avatar_url} alt={'GitHub user：' + props.owner.login} width='32px' height='32px' />
+        <span className='text-dark-gray fs-5 fw-light'>{props.owner.login}</span>
+      </Link>
       <Link className='text-light-gray' to={'/users/' + props.owner.login + '/repos'}>
         <svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' fill='currentColor' className='bi bi-x-lg' viewBox='0 0 14 14'>
           <path fillRule='evenodd' d='M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z' />
