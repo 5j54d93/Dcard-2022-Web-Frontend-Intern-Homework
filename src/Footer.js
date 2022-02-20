77bc6fb97fa0129e0637ddd14ce2ac7277467a9c
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-20 16:52:50 +0800

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
         <p className='mb-1 text-center'>Dcard 2022 Web Frontend Intern Homework</p>
         <p className='mb-0 text-center'>
           Copyright © 2022 Ricky Chuang｜
-          <a href='/'>首頁</a>｜
-          <a href='https://linktr.ee/5j_54d93' target='_blank' rel="noreferrer">社群連結</a>｜
-          <a href='https://github.com/5j54d93/Dcard-2022-Web-Frontend-Intern-Homework' target='_blank' rel="noreferrer">GitHub</a>｜
+          <a href='/'>HOME</a>｜
+          <a href='https://github.com/5j54d93/Dcard-2022-Web-Frontend-Intern-Homework' target='_blank' rel='noreferrer'>GitHub</a>｜
+          <a href='https://linktr.ee/5j_54d93' target='_blank' rel='noreferrer'>Linktree</a>｜
           Designed by Ricky Chuang
         </p>
       </div>
