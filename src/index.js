708b5fb80256d8367b359e2df16e81e3793caaeb
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-20 16:53:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Search from './Search';
-import RepoList from './RepoList';
+import UserPage from './UserPage';
 import RepoDetail from './RepoDetail';
 import Footer from './Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="d-flex flex-column min-vh-100">
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Search />} />
-          <Route path="/users/:owner/repos" element={<RepoList />} />
-          <Route path="/users/:owner/repos/:repo" element={<RepoDetail />} />
+          <Route path='/' element={<Search />} />
+          <Route path='/users/:owner/repos' element={<UserPage />} />
+          <Route path='/users/:owner/repos/:repo' element={<RepoDetail />} />
         </Routes>
       </BrowserRouter>
       <Footer />
