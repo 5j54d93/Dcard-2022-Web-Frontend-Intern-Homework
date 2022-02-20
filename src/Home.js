0e60206ceb107425ef8ed5255cf95e34e98971eb
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-20 22:06:26 +0800

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -41,7 +41,7 @@ function Search() {
         GitHub Username
       </label>
       <div className='input-group'>
-        <input type='search' className='form-control form-control-lg' placeholder='Search dcard' aria-label='GitHub username' aria-describedby='GitHubUsername' value={username} onChange={onChangeHandler} autoFocus={true} required />
+        <input type='search' className='form-control form-control-lg' placeholder='Search Dcard' aria-label='GitHub username' aria-describedby='GitHubUsername' value={username} onChange={onChangeHandler} autoFocus={true} required />
         <button className='btn btn-secondary btnSearch' type='submit' aria-label="search a GitHub user's repositories">
           <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' className='bi bi-search mx-1' viewBox='0 0 16 16'>
             <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
