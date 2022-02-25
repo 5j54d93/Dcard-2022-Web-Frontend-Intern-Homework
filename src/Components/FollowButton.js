35e50739283a291ccdb9d574b878e887e78bf837
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-26 02:14:03 +0800

    Update FollowButton.js

--- src/Components/FollowButton.js
+++ src/Components/FollowButton.js
@@ -1,38 +1,39 @@
 import React, { useState, useEffect } from 'react';
 
+import styles from '../Styles/FollowButton.module.css';
+
 export default function FollowButton(props) {
   const [following, setFollowing] = useState(false);
 
   useEffect(() => {
     if (localStorage.followingUsers !== '[]') {
-      var followingUsers = JSON.parse(localStorage.getItem('followingUsers'));
-      for (let i = 0; i < followingUsers.length; i++) {
-        if (followingUsers[i].login === props.userData.login) {
+      const followingUsers = JSON.parse(localStorage.getItem('FollowingUsers'));
+      followingUsers.forEach(followingUser => {
+        if (followingUser.login === props.userData.login) {
           setFollowing(true);
         }
-      }
+      });
     }
-  }, [props.userData.login])
+  })
 
   function handleClick() {
     if (following) {
-      let followingUsers = JSON.parse(localStorage.getItem('followingUsers'));
+      let followingUsers = JSON.parse(localStorage.getItem('FollowingUsers'));
       followingUsers.forEach((followingUser, index) => {
         if (followingUser.login === props.userData.login) {
           followingUsers.splice(index, 1);
-          localStorage.setItem('followingUsers', JSON.stringify(followingUsers));
-        };
+          localStorage.setItem('FollowingUsers', JSON.stringify(followingUsers));
+        }
       })
     } else {
-      localStorage.setItem('followingUsers', JSON.stringify(JSON.parse(localStorage.getItem('followingUsers')).concat([props.userData])));
+      localStorage.setItem('FollowingUsers', JSON.stringify(JSON.parse(localStorage.getItem('FollowingUsers')).concat([props.userData])));
     }
-
     setFollowing(!following);
   }
 
   return (
     following
-      ? <div className='btn-light-gray' onClick={handleClick} role='button'>Following</div>
-      : <div className='btn-light-blue' onClick={handleClick} role='button'>Follow</div>
+      ? <div className={styles.btnFollowing} onClick={handleClick} role='button'>Following</div>
+      : <div className={styles.btnFollow} onClick={handleClick} role='button'>Follow</div>
   );
 }
