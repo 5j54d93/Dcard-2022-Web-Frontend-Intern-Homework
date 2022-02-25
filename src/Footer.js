01fa83527af58d64cbbf54b41e196ca12ea9a6e4
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-26 02:17:08 +0800

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,12 @@
+import styles from './Styles/Footer.module.css';
+
 export default function Footer() {
   return (
-    <footer className='border-top text-light-white py-3'>
-      <div className='container'>
-        <p className='mb-1 text-center'>Dcard 2022 Web Frontend Intern Homework</p>
-        <p className='mb-0 text-center'>
-          Copyright © 2022 Ricky Chuang｜
-          <a href='/'>HOME</a>｜
-          <a href='https://github.com/5j54d93/Dcard-2022-Web-Frontend-Intern-Homework' target='_blank' rel='noreferrer'>GitHub</a>｜
-          <a href='https://linktr.ee/5j_54d93' target='_blank' rel='noreferrer'>Linktree</a>｜
-          Designed by Ricky Chuang
-        </p>
-      </div>
+    <footer className={`container d-flex flex-wrap align-items-center lh-lg py-4 ${styles.footer}`}>
+      <a href='/'>Home</a>
+      <a className='mx-3' href='https://github.com/5j54d93/Dcard-2022-Web-Frontend-Intern-Homework' target='_blank' rel='noreferrer'> GitHub</a>
+      <a className='me-auto' href='https://linktr.ee/5j_54d93' target='_blank' rel='noreferrer'> Linktree</a>
+      Copyright © Ricky Chuang 2022
     </footer>
   );
 }
