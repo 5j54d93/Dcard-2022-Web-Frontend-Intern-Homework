06809de3da279282db0f8972819f96b529299145
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-26 02:14:20 +0800

    Update IconGroup.js

--- src/Components/IconGroup.js
+++ src/Components/IconGroup.js
@@ -1,20 +1,25 @@
 import React, { memo } from 'react';
 
+import styles from '../Styles/IconGroup.module.css';
+
 export default memo(function IconGroup(props) {
   return (
     <>
-      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='starIcon bi bi-star-fill' viewBox='0 0 16 16'>
+      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className={`bi bi-star-fill ${styles.starIcon}`} viewBox='0 0 16 16'>
         <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
       </svg>
-      <span className='me-3'>{props.starsCount}</span>
-      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='forkIcon bi bi-diagram-2-fill' viewBox='0 0 16 16'>
+      <span className='me-3'>{props.repoData.stargazers_count}</span>
+      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className={`bi bi-diagram-2-fill ${styles.forkIcon}`} viewBox='0 0 16 16'>
         <path fillRule='evenodd' d='M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 5 7h2.5V6A1.5 1.5 0 0 1 6 4.5v-1zm-3 8A1.5 1.5 0 0 1 4.5 10h1A1.5 1.5 0 0 1 7 11.5v1A1.5 1.5 0 0 1 5.5 14h-1A1.5 1.5 0 0 1 3 12.5v-1zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1A1.5 1.5 0 0 1 9 12.5v-1z' />
       </svg>
-      <span className='me-3'>{props.forksCount}</span>
-      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='codeIcon bi bi-code-slash' viewBox='0 0 16 16'>
+      <span className='me-3'>{props.repoData.forks_count}</span>
+      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className={`bi bi-code-slash ${styles.languageIcon}`} viewBox='0 0 16 16'>
         <path d='M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z' />
       </svg>
-      <span className='me-auto'>{props.language}</span>
+      {props.repoData.language
+        ? <span className='me-auto'>{props.repoData.language}</span>
+        : <span className='me-auto'>Null</span>
+      }
     </>
   );
 })
