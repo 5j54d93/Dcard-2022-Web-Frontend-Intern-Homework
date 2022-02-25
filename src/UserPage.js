1fcb4d029b7202d2cf71baa2f7220e5dd7d47b27
Ricky Chuang <songsongyyohya@gmail.com>
2022-02-26 02:18:43 +0800

    Update UserPage.js

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect, memo } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef, memo } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import IconGroup from './Components/IconGroup';
 import CreateTime from './Components/CreateTime';
 import FollowButton from './Components/FollowButton';
-import IconGroup from './Components/IconGroup';
+
+import styles from './Styles/UserPage.module.css';
 
 export default function UserPage() {
   const userData = JSON.parse(sessionStorage.getItem('GitHubUser'));
 
   return (
     <div className='d-flex flex-fill align-items-center'>
-      <div className='container backContainer my-4'>
-        <div className='frontContainer'>
-          <main className='userPageContent'>
-            <GitHubUser />
-            <div className='container repoRowContainer p-4 mt-2'>
-              {userData.message === 'Not Found'
-                ? <div className='fs-3 text-center text-middle-blue'>No Such User.</div>
-                : userData.public_repos === 0
-                  ? <div className='fs-3 text-center text-middle-blue'>Haven't created any repository yet.</div>
-                  : <RepoList />
-              }
-            </div>
-          </main>
-        </div>
+      <div className={`container ${styles.bgLightGrayContainer}`}>
+        <div className={styles.banner} />
+        <main className={styles.userPage}>
+          <GitHubUser />
+          <div className={`p-4 mt-2 ${styles.bgWhiteContainer}`}>
+            {userData.message === 'Not Found'
+              ? <div className={`fs-3 text-center ${styles.textMiddleBlue}`}>No Such User.</div>
+              : userData.public_repos === 0
+                ? <div className={`fs-3 text-center ${styles.textMiddleBlue}`}>Haven't created any repository yet.</div>
+                : <RepoList />
+            }
+          </div>
+        </main>
       </div>
     </div>
   );
@@ -34,33 +35,31 @@ function GitHubUser() {
 
   return (
     <>
-      <img className='ms-2 my-3' src={userData.avatar_url} alt={userData.name} width='142px' height='142px' style={{ backgroundColor: 'white', borderRadius: '50%', border: '6px solid rgb(242,243,244)' }} />
-      <header className='d-flex align-items-end ms-2'>
-        <div className='text-dark fs-5 fw-bold lh-1 me-auto'>
-          {userData.name}
-          <br />
-          <span className='text-light-gray fw-light' style={{ fontSize: '0.9rem' }}>
-            @{userData.login}
-            <br />
+      <img className={`ms-2 mb-2 ${styles.avartar}`} src={userData.avatar_url} alt={userData.name} width='142px' height='142px' />
+      <header className='d-flex flex-wrap align-items-end ms-2'>
+        <div className='me-auto lh-1'>
+          <h1 className={styles.name}>{userData.name}</h1>
+          <span className={`fw-light ${styles.userDetail}`}>
+            <h2 className={styles.username}>@{userData.login}</h2>
             {userData.type === 'Organization'
               ?
               <>
                 {userData.location &&
-                  <span className='d-flex align-items-center mt-1'>
-                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-geo-alt' viewBox='0 0 16 16'>
+                  <span className='d-flex align-items-center'>
+                    <svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' fill='currentColor' className='bi bi-geo-alt' viewBox='0 0 17 17'>
                       <path d='M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z' />
                       <path d='M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z' />
                     </svg>
-                    {userData.location}
+                    <span className={styles.username}>{userData.location}</span>
                   </span>
                 }
                 {userData.blog &&
-                  <span className='d-flex align-items-center mt-1'>
+                  <span className='d-flex align-items-center'>
                     <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-link-45deg' viewBox='0 0 16 16'>
                       <path d='M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z' />
                       <path d='M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z' />
                     </svg>
-                    <a className='text-light-gray d-inline-block text-truncate' href={userData.blog} target='_blank' rel='noreferrer' style={{ maxWidth: '11rem', cursor: 'pointer' }} >
+                    <a className={`d-inline-block text-truncate ${styles.blogLink}`} href={userData.blog} target='_blank' rel='noreferrer' >
                       {userData.blog}
                     </a>
                   </span>
@@ -72,61 +71,49 @@ function GitHubUser() {
           </span>
         </div>
         {userData.message === 'Not Found'
-          ? <a className='btn-light-blue' href='/' role='button'>Search another User</a>
-          : <FollowButton userData={userData} />
+          ? <a className={`mt-2 ${styles.btnSearchAnotherUser}`} href='/' role='button'>Search another User</a>
+          : <span className='mt-2'><FollowButton userData={userData} /></span>
         }
       </header>
-      <p className='text-dark-gray fs-6 fw-light mt-3'>{userData.bio}</p>
+      <p className={styles.bio}>{userData.bio}</p>
       <hr />
       {userData.type === 'Organization'
-        ? <span className='text-dark-gray' style={{ fontSize: '0.8rem' }}>{userData.public_repos} Public Repositories</span>
-        : <span className='text-dark-gray' style={{ fontSize: '0.8rem' }}>All Public Repositories</span>
+        ? <span className={styles.repoListTag}>{userData.public_repos} Public Repositories</span>
+        : <span className={styles.repoListTag}>All Public Repositories</span>
       }
     </>
   );
 }
 
-// API call
-function FetchRepos(username, page) {
-  const [repos, setRepos] = useState([]);
-
-  useEffect(() => {
-    const fetchRepos = async () => {
-      try {
-        const response = await fetch('https://api.github.com/users/' + username + '/repos?sort=created&per_page=10&page=' + page);
-        const json = await response.json();
-        setRepos([...repos, ...json]);
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
-    fetchRepos();
-  }, [username, page]);
-
-  return repos;
-}
-
 function RepoList() {
-  const [page, setPage] = useState(1);
+  const progressRef = useRef(null);
+  const [page, setPage] = useState(Number(sessionStorage.getItem('page')));
   const userData = JSON.parse(sessionStorage.getItem('GitHubUser'));
-  const repoData = FetchRepos(userData.login, page);
+  const repoData = JSON.parse(sessionStorage.getItem('Repos'));
+
+  async function fetchRepos(page) {
+    const response = await fetch('https://api.github.com/users/' + userData.login + '/repos?sort=created&per_page=10&page=' + page);
+    const json = await response.json();
+    sessionStorage.setItem('Repos', JSON.stringify(JSON.parse(sessionStorage.getItem('Repos')).concat(json)));
+    sessionStorage.setItem('page', page);
+    sessionStorage.setItem('offsetY', document.documentElement.scrollTop);
+    setPage(page);
+  }
 
   useEffect(() => {
-    function infiniteScroll() {
-      if (
-        Math.round(window.innerHeight + document.documentElement.scrollTop) === document.documentElement.offsetHeight
-        &&
-        10 * page < userData.public_repos
-      ) {
-        setTimeout(() => {
-          setPage(page + 1);
-        }, 1000)
+    window.scroll(0, sessionStorage.getItem('offsetY'));
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && 10 * page < userData.public_repos) {
+        fetchRepos(page + 1);
       }
+    }, { root: null, rootMargin: '0px', threshold: 1.0 });
+
+    if (progressRef.current) {
+      observer.observe(progressRef.current);
     }
-    window.addEventListener('scroll', infiniteScroll);
     return () => {
-      window.removeEventListener('scroll', infiniteScroll);
-    };
+      observer.disconnect();
+    }
   })
 
   return (
@@ -136,13 +123,13 @@ function RepoList() {
       ))}
       {10 * page < userData.public_repos
         ?
-        <div className='text-center text-middle-blue'>
-          <div className='spinner-border align-middle' role='status'>
-            <span className='visually-hidden'>Loading...</span>
+        <div className={`text-center ${styles.textMiddleBlue}`}>
+          <div className='spinner-border' role='status'>
+            <span className='visually-hidden' ref={progressRef}>Loading...</span>
           </div>
         </div>
         :
-        <div className='fs-5 text-center text-middle-blue'>No more repository</div>
+        <div className={`fs-5 text-center ${styles.textMiddleBlue}`}>No more repositories.</div>
       }
     </>
   );
@@ -150,22 +137,42 @@ function RepoList() {
 
 const RepoRow = memo(function RepoRow(props) {
   const userData = JSON.parse(sessionStorage.getItem('GitHubUser'));
+  const navigate = useNavigate();
+
+  async function fetchData() {
+    const response = await fetch('https://api.github.com/repos/' + userData.login + '/' + props.repo.name);
+    const json = await response.json();
+    sessionStorage.setItem('repoDetail', JSON.stringify(json));
+    navigate(props.repo.name);
+  }
+
+  function handleClick() {
+    sessionStorage.setItem('offsetY', document.documentElement.scrollTop);
+    if (JSON.parse(sessionStorage.getItem('repoDetail')).name !== props.repo.name) {
+      fetchData();
+    } else {
+      navigate(props.repo.name);
+    }
+  }
 
   return (
-    <Link to={props.repo.name}>
-      <div className='d-flex flex-wrap align-items-center gap-2 mb-2'>
-        <img className='img-fluid' src={userData.avatar_url} alt={userData.name} width='20px' height='20px' style={{ borderRadius: '50%' }} />
-        <span className='text-dark-gray fs-6 fw-light me-auto' style={{ fontSize: '0.8rem' }}>{userData.name}</span>
-        <span className='text-dark-gray fs-6 fw-light' style={{ fontSize: '0.8rem' }}>
+    <div className={styles.repoRow} to={props.repo.name} onClick={() => handleClick()}>
+      <div className='d-flex align-items-center gap-2 mb-2'>
+        <img className={`img-fluid ${styles.roundImage}`} src={userData.avatar_url} alt={userData.name} width='16px' height='16px' />
+        <span className={`me-auto ${styles.repoRowOwnerTag}`}>{userData.name}</span>
+        <span className={styles.repoRowOwnerTag}>
           <CreateTime created_at={props.repo.created_at} displayTime={false} />
         </span>
       </div>
-      <p className='text-dark fs-5 fw-bold'>{props.repo.name}</p>
-      <p className='text-black mb-3' style={{ fontSize: '0.9rem' }}>{props.repo.description}</p>
-      <div className='text-light-gray d-flex align-items-center gap-2' style={{ fontSize: '0.8rem' }}>
-        <IconGroup starsCount={props.repo.stargazers_count} forksCount={props.repo.forks_count} language={props.repo.language} />
+      <p className={styles.repoName}>{props.repo.name}</p>
+      {props.repo.description
+        ? <p className={styles.description}>{props.repo.description}</p>
+        : <p className={styles.description}>No description</p>
+      }
+      <div className={`d-flex align-items-center gap-2 ${styles.iconGroup}`}>
+        <IconGroup repoData={props.repo} />
       </div>
-      <hr className='text-dark-gray' />
-    </Link>
+      <hr className={styles.hr} />
+    </div>
   );
 })
